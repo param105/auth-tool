92ce4f3632f4b526402a7f46f369b56b07662a73
Parmeshwar <parmeshwar@bruviti.com>
2020-06-10 10:54:04 +0530

    graph butification

--- web-react/src/components/Graphs.js
+++ web-react/src/components/Graphs.js
@@ -29,9 +29,9 @@ export default function Graphs() {
   if (error) return <p>Error</p>
   if (loading) return <p>Loading</p>
 
-  function getNode(id, label) {
+  function getNode(id, label, color) {
     return {
-      data: { id: id, label: label },
+      data: { id: id, label: label, color: color },
       position: {
         x: Math.floor(Math.random() * 900) + 50,
         y: Math.floor(Math.random() * 400) + 50,
@@ -42,9 +42,11 @@ export default function Graphs() {
   function getElements() {
     const elements = []
     data.Review.map((review) => {
-      elements.push(getNode(review._id, review._id))
-      elements.push(getNode(review.business._id, review.business.name))
-      elements.push(getNode(review.user._id, review.user.name))
+      elements.push(getNode(review._id, review._id, '#FF5733'))
+      elements.push(
+        getNode(review.business._id, review.business.name, '#33FF49')
+      )
+      elements.push(getNode(review.user._id, review.user.name, '#3342FF'))
       elements.push({
         data: { source: review._id, target: review.business._id },
       })
@@ -57,7 +59,28 @@ export default function Graphs() {
     <div>
       <CytoscapeComponent
         elements={getElements()}
+        layout={{ name: 'breadthfirst', padding: 10 }}
         style={{ width: '1000px', height: '600px' }}
+        stylesheet={[
+          {
+            selector: 'node',
+            style: {
+              height: 80,
+              width: 80,
+              backgroundFit: 'cover',
+              borderColor: 'data(color)',
+              borderWidth: 10,
+              borderOpacity: 0.5,
+              label: 'data(label)',
+            },
+          },
+          {
+            selector: 'edge',
+            style: {
+              width: 4,
+            },
+          },
+        ]}
       />
       ;
     </div>
